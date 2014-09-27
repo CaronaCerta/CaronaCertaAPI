261553593be96f9ee54943c44e797cfd390b321c
Rodrigo Elizeu Goncalves <rodrigoegoncalves@gmail.com>
2014-09-27 18:50:51 -0300

    MVC version test

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit3e4c5b221d7cf897f076aa136ac11d46::getLoader();
+return ComposerAutoloaderInit6566fc6fe50c6dd04abbfcc2a00c8dd0::getLoader();
