5a270d34b5466ada344f843ede3608c747b0281a
Rodrigo Elizeu Goncalves <rodrigoegoncalves@gmail.com>
2014-11-24 22:16:09 -0200

    Removing docs

--- docs/api_project.js
+++
@@ -1,9 +0,0 @@
-define({
-  "name": "carona-certa",
-  "version": "0.1.0",
-  "description": "Carona Certa API documentation",
-  "generator": {
-    "version": "0.5.0",
-    "time": "2014-11-24T02:04:43.395Z"
-  }
-});
